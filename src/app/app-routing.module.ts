70d18b6320e2c08f5b896565f92aebd407398b85
Jose Freddy <dev.fherrera@gmail.com>
2022-12-13 20:39:54 -0600

    creacion de componentes y router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BuscadorComponent } from './Components/buscador/buscador.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { HomeComponent } from './Components/home/home.component';
+import { NavComponent } from './Components/nav/nav.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'buscador',component:BuscadorComponent},
+  {path:'nav',component:NavComponent},
+  {path:'footer',component:FooterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { NavComponent } from './Components/nav/nav.component';
+import { BuscadorComponent } from './Components/buscador/buscador.component';
+import { FooterComponent } from './Components/footer/footer.component';
+
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    BuscadorComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
